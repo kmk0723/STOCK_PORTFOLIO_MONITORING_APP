373c6a294c5d04e816aefca0fd5f10df90f5a893
kmk0723 <manikarthikeya1@gmail.com>
2025-05-26 12:19:15 +0530

    [Karthik] Files Added

---
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/StockPortfolioMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.Stock.Portfolio.Monitoring.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/Stock/Portfolio/Monitoring/App/StockPortfolioMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.Stock.Portfolio.Monitoring.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
