39b021c9800f24b69230c06627c2d6882f0e1e83
Nithin <cnithinkumar123@gmail.com>
2025-05-27 14:18:11 +0530

    [Nithin] added Profolio Model

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/Portfolio.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/Portfolio.java
@@ -1,5 +1,14 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.model;
-
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 public class Portfolio {
+	
+	private long id;
+	private String name;
+	private String Description;
+	private User user;
+	private LocalDateTime createdAt = LocalDateTime.now();
+	
+	
 
 }
