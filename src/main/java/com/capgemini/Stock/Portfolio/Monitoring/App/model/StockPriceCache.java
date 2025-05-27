5382c31e3dac8ba294d87f13813440e2ab1c5673
VishnuVimal24 <vishnuvim04@gmail.com>
2025-05-27 15:01:27 +0530

    [Vishnu] Added StockPriceCache Model

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/StockPriceCache.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/StockPriceCache.java
@@ -1,5 +1,45 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.model;
 
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity 
+@Table(name = "stock_price_cache")
 public class StockPriceCache {
+	@Id 
+	private String stockSymbol;
+	
+	private double price;
+	private LocalDateTime lastUpdated;
+	
+	public StockPriceCache() {
+		
+	}
+	public StockPriceCache(String stockSymbol, double price, LocalDateTime lastUpdated) {
+		this.stockSymbol = stockSymbol;
+		this.price = price;
+		this.lastUpdated= lastUpdated;
+	}
+	
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+	
+	public void setStockSymbol(String stockSymbol) {
+		this.stockSymbol = stockSymbol;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price =price;
+	}
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
 
 }
