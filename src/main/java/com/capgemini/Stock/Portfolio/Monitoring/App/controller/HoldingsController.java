57db1c02a7a6db669512fbe0c6e730811716b168
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-27 12:31:06 +0530

    [Bevin] created Model package

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/controller/HoldingsController.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/controller/HoldingsController.java
@@ -1,5 +1,16 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.controller;
 
-public class HoldingsController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/holdings")
+public class HoldingsController {
+	
+	@GetMapping
+	public String saveHoldings() {
+		return "saa";
+	}
 }
---
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/StockPriceCache.java
@@ -0,0 +1,5 @@
+package com.capgemini.Stock.Portfolio.Monitoring.App.model;
+
+public class StockPriceCache {
+
+}
