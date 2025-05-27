f41c949a35837373196097889c9b45c308ef7b41
sanjeevknsty <121655617+sanjeevknsty@users.noreply.github.com>
2025-05-27 15:19:29 +0530

    [Nithin] Updated Portfolio java

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/Portfolio.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/model/Portfolio.java
@@ -1,14 +1,5 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.model;
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+
 public class Portfolio {
-	
-	private long id;
-	private String name;
-	private String Description;
-	private User user;
-	private LocalDateTime createdAt = LocalDateTime.now();
-	
-	
 
 }
