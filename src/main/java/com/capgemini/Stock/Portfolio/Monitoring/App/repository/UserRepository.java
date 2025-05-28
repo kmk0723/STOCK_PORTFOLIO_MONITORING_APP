7a168d2d58f0f49b3db692b4760fc77810dd77ec
bevinjoshua <bevinj121@gmail.com>
2025-05-28 16:55:10 +0530

    [Bevin] extended JPA rep to User rep and added methods

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/UserRepository.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capgemini.Stock.Portfolio.Monitoring.App.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
 }
