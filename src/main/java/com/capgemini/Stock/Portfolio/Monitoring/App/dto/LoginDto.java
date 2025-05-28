2918c304d490499c08437b3ade885116f11d95d0
VishnuVimal24 <vishnuvim04@gmail.com>
2025-05-28 17:02:48 +0530

    [Vishnu] Created Login Dto

---
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/dto/LoginDto.java
@@ -0,0 +1,29 @@
+package com.capgemini.Stock.Portfolio.Monitoring.App.dto;
+
+public class LoginDto {
+
+	 private String username;
+	    private String password;
+	    
+	    public LoginDto(String username ,String password) {
+	    	this.username = username;
+	    	this.password  = password;
+	    }
+
+	    // Getters and Setters
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+}
