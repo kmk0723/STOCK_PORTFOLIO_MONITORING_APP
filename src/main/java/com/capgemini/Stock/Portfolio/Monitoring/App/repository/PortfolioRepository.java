1d134e7e24b1f014faa4f960a785a60520a7718a
Sri Veer <bandaru.saisreeveer@gmail.com>
2025-05-29 11:50:53 +0530

    [Sri Veer] Updated User Controller, Repository and Service

--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/PortfolioRepository.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/PortfolioRepository.java
@@ -1,5 +1,9 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.repository;
 
+import com.capgemini.Stock.Portfolio.Monitoring.App.model.Portfolio;
+
 public interface PortfolioRepository {
+
+	void save(Portfolio portfolio);
 
 }
--- src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/UserRepository.java
+++ src/main/java/com/capgemini/Stock/Portfolio/Monitoring/App/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.capgemini.Stock.Portfolio.Monitoring.App.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.capgemini.Stock.Portfolio.Monitoring.App.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    boolean existsByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
